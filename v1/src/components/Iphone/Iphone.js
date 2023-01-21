013103df3edf85507e9023e71ed691d1c9413ee1
박병관 <85085375+codingbotPark@users.noreply.github.com>
2023-01-21 13:28:35 +0900

    [CHECKPOINNT]

--- v1/src/components/Iphone/Iphone.js
+++ v1/src/components/Iphone/Iphone.js
@@ -3,19 +3,44 @@ import I from "./Iphone.style.scss"
 class Iphone extends HTMLElement{
     connectedCallback() {
         this.innerIphone()
+        this.outerIphone()
     }
 
     innerIphone(){
+        this.phoneFrame()
+        this.dynamicIsland()
+    }
+
+    outerIphone(){
+        
+    }
+
+    phoneFrame(){
         let bezel = document.createElement('div')
         bezel.className = I.bezel
         this.appendChild(bezel)
 
         let iphoneDisplay = document.createElement('div')
         iphoneDisplay.className = I.display
-        // iphoneDisplay.style.width="100px"
-        // iphoneDisplay.style.height="100px"
-        // iphoneDisplay.style.backgroundColor="yellow"
         bezel.appendChild(iphoneDisplay)
+    }
+
+    dynamicIsland(){
+        let dynamicIslandWrapper = document.createElement('div')
+        dynamicIslandWrapper.className = I.dynamicIslandWrapper
+        this.appendChild(dynamicIslandWrapper)
+
+        let dynamicIsland = document.createElement('div')
+        dynamicIsland.className = I.dynamicIsland
+        dynamicIslandWrapper.appendChild(dynamicIsland)
+
+        let selfCamera = document.createElement('div')
+        selfCamera.className = I.selfCamera
+        dynamicIsland.appendChild(selfCamera) 
+
+        let selfCameraLens = document.createElement('div')
+        selfCameraLens.className = I.selfCameraLens
+        selfCamera.appendChild(selfCameraLens)
     }
 
 }
--- v1/src/index.js
+++ v1/src/index.js
@@ -1,5 +1,7 @@
+// import "./style/_variable.scss"
 import "./index.style.scss"
 import App from "./App"
 
 const body = document.getElementsByTagName('body')[0]
 body.appendChild(App)
+
