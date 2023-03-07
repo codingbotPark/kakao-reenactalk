22fd12cf87d9524c0c989d96a378e9b7ea24578b
박병관 <85085375+codingbotPark@users.noreply.github.com>
2023-03-07 14:10:33 +0900

    [CHECKPOINT] for navBar

---
+++ v1/src/components/common/NavBar/NavBar.js
@@ -0,0 +1,9 @@
+import customElement from "../../../classes/CustomElementClass";
+import ui from "./ui";
+import N from "./NavBar.style.scss"
+
+class NavBar extends customElement{
+    connectedCallback(){
+        
+    }
+}
